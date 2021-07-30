2ae2ea5221e05457dced24a9f3f332e1f78422f0
vlad-dvlad <vlad.ivashko72@gmail.com>
2021-07-30 12:33:07 +0300

    add null styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from './components/header/Header';
+
 
 function App() {
-  
+    return (
+        <div className="wrapper">
+            <Header />
+        </div>
+    )
 }
 
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './header.scss'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className="header__logo">
+                {/* image */}
+            </div>
+            <div className="header__title">
+                <h1>Cool Network</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
