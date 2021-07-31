a5b1a4e6e83d05537722c15c2b1469ba3394a2e9
vlad-dvlad <vlad.ivashko72@gmail.com>
2021-07-31 15:02:44 +0300

    create page component

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../navbar/Navbar';
+import Page from '../page/Page';
 import './content.scss'
 
 const Content = () => {
     return (
         <main className="main main__container">
             <Navbar />
+            <Page />
         </main>
         
     );
---
+++ src/components/page/Page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './page.scss';
+
+const Page = () => {
+    return (
+        <div className="page page__container">
+
+        </div>
+    );
+}
+
+export default Page;
