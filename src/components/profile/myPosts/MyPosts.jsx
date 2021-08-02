9463da6725876e06bc54844544ceea35a9bbc75c
vlad-dvlad <vlad.ivashko72@gmail.com>
2021-08-02 10:38:38 +0300

    post template

--- src/components/profile/myPosts/MyPosts.jsx
+++ src/components/profile/myPosts/MyPosts.jsx
@@ -5,6 +5,8 @@ import Post from './Posts/Post';
 const MyPosts = () => {
     return(
         <div className={`${classes.posts} ${classes.posts__container}`}>
+            <textarea className={classes.posts__text}></textarea>
+            <button>Add post</button>
              <div>My post</div>
              <Post />
              <Post />
--- src/components/profile/myPosts/Posts/Post.jsx
+++ src/components/profile/myPosts/Posts/Post.jsx
@@ -4,10 +4,13 @@ import classes from './post.module.scss'
 const Post = () => {
     return (
         <div>
-                 <textarea className={classes.post__text}></textarea>
-                 <button>Add post</button>
+                 
                  <div className={classes.post__image}>
+                     <div>Post about something</div>
                      <img src="https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop" alt="" />
+                    <div>
+                        <span>like</span>
+                    </div>
                  </div>
              </div>
     );
