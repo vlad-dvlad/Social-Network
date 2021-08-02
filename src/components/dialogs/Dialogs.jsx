ff3d402966bbd98f35b19518f9634ab70a439fc8
vlad-dvlad <vlad.ivashko72@gmail.com>
2021-08-02 19:30:39 +0300

    dialogs

---
+++ src/components/dialogs/Dialogs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './dialogs.module.scss';
+
+const Dialogs = (props) => {
+    return (
+        <div className={`${classes.dialogs} ${classes.dialogs__container}`}>
+            <div>
+                Dialogs
+            </div>
+        </div>
+    );
+}
+
+export default Dialogs;
