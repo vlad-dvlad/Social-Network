a0b84debbaf1fd6f75e317453dac5b140e1d1db4
vlad-dvlad <vlad.ivashko72@gmail.com>
2021-08-05 11:43:52 +0300

    create burger icon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,13 +4,19 @@ import classes from './header.module.scss';
 
 const Header = () => {
     return (
-        <header className={`${classes.header} ${classes.header__container}`}>
-            <div className={classes.header__logo}>
-                <img src={process.env.PUBLIC_URL + '/logo.png'} alt="" />
+        <header className={classes.header}>
+            <div className={classes.header__container}>
+                <div className={classes.header__logo}>
+                    <img src={process.env.PUBLIC_URL + '/logo.png'} alt="" />
+                </div>
+                <div className={classes.header__title}>
+                    <h1><a href="javascript:void(0);">Cool Network</a></h1>
+                </div>
+                <div className={classes.header__icon}>
+                    <span></span>
+                </div>
             </div>
-            <div className={classes.header__title}>
-                <h1><a href="javascript:void(0);">Cool Network</a></h1>
-            </div>
+            
         </header>
     )
 }
