4bed879d4c023860e7c419fe6eda2b5a65a0dc40
vlad-dvlad <vlad.ivashko72@gmail.com>
2021-08-11 10:27:05 +0300

    Create simple design for dialogs items

--- src/components/dialogs/dialogItem/DialogItem.jsx
+++ src/components/dialogs/dialogItem/DialogItem.jsx
@@ -6,9 +6,11 @@ const DialogItem = (props) => {
     let path = "/dialogs/" + props.id;
     return (
         <div className={classes.dialogs__dialog}>
-            <div className={classes.person__avatar}></div>
+            <div className={classes.person__avatar}>
+                <img src="https://cdn.britannica.com/q:60/92/181392-050-08187DB5/cat-British-Shorthair-grey.jpg" alt=""/>
+            </div>
             <div className={classes.person__name}>
-                <NavLink to={path}>{props.name}</NavLink>
+                <NavLink className={classes.person__text} to={path}>{props.name}</NavLink>
             </div>
         </div>
     );
