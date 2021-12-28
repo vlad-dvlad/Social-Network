951a85dc08bbe92ae5f1d62260a82c6e3090ba10
vlad-dvlad <vlad.ivashko72@gmail.com>
2021-12-28 09:30:52 +0200

    Refactoring MyPosts.jsx

--- src/components/profile/myPosts/MyPosts.jsx
+++ src/components/profile/myPosts/MyPosts.jsx
@@ -5,22 +5,9 @@ import {reduxForm} from "redux-form";
 import {createField, Textarea} from "../../common/controlForms/ControlForms";
 import {maxLengthCreator, required} from "../../../utils/validators/validators";
 
-
 const maxLength50 = maxLengthCreator(50);
 
 const MyPosts = React.memo(({posts, profile, addPostClear}) => {
-
-    if(!profile) {
-        return <></>;
-    }
-
-    let postsElement = posts.map(p => {
-        return (
-            <div className={styles.posts__item}>
-                <Post profile={profile} message={p.message} likeCount={p.likesCount} key={p.id}/>
-            </div>
-        );
-    });
 
     const onAddPost = (formData) => {
         addPostClear(formData.newPost);
@@ -30,16 +17,26 @@ const MyPosts = React.memo(({posts, profile, addPostClear}) => {
         <div className={`${styles.posts} ${styles.posts__container}`}>
             <MyPostsReduxForm onSubmit={onAddPost}/>
             <div className={styles.posts__title}>My post</div>
-            <div className={styles.posts__items}>
-                {
-                    posts.map(p => <div className={styles.posts__item}>
-                        <Post photos={profile.photos} message={p.message} likeCount={p.likesCount} key={p.id}/>
-                    </div>)
-                }
-            </div>
+            <CreatePost posts={posts} profile={profile}/>
         </div>
     );
 });
+
+const CreatePost = ({posts, profile}) => {
+    if(!profile) {
+        return <></>;
+    }
+
+    return (
+        <div className={styles.posts__items}>
+            {
+                posts.map(p => <div className={styles.posts__item}>
+                    <Post photos={profile.photos} message={p.message} likeCount={p.likesCount} key={p.id}/>
+                </div>)
+            }
+        </div>
+    );
+}
 
 const MyPostsForm = ({handleSubmit}) => {
     return (
