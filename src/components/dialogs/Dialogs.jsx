be97a719d5b4752e16ad777f90cd5951616456ce
vlad-dvlad <vlad.ivashko72@gmail.com>
2021-12-30 13:57:03 +0200

    Create responsive for message

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -1,46 +1,39 @@
 import React from 'react';
-import classes from './dialogs.module.scss';
+import styles from './dialogs.module.scss';
 import DialogItem from "./dialogItem/DialogItem";
 import Message from "./message/Message";
-import {Redirect} from "react-router-dom";
-import {Field, reduxForm} from "redux-form";
+import {reduxForm} from "redux-form";
 import {maxLengthCreator, required} from "../../utils/validators/validators";
-import {Textarea} from "../common/controlForms/ControlForms";
+import {createField, Textarea} from "../common/controlForms/ControlForms";
 
 
-const maxLength30 = maxLengthCreator(30);
+const maxLength100 = maxLengthCreator(100);
 
-const Dialogs = ({dialogs, messages, send, isAuth}) => {
+const Dialogs = ({dialogs, messages, send}) => {
 
-    let dialogsElement = dialogs.map(d => <DialogItem name={d.name} id={d.id} key={d.id}/>);
-    let messagesElements = messages.map(m => <Message message={m.message} key={m.id}/>);
+    const dialogsElement = dialogs.map(d => <DialogItem name={d.name} id={d.id} key={d.id}/>);
+    const messagesElements = messages.map(m => <Message message={m.message} key={m.id}/>);
 
     const onSendMessage = (formData) => {
         send(formData.newMessage);
     }
 
-    if(!isAuth) {
-        return <Redirect to={'/login'}/>
-    }
-
-
     return (
-        <div className={classes.dialogs}>
-            <div className={classes.dialogs__container}>
-                <div className={classes.dialogs__items}>
+        <div className={styles.dialogs}>
+            <div className={styles.dialogs__container}>
+                <div className={styles.dialogs__items}>
                     {dialogsElement}
                 </div>
-                <div className={classes.dialogs__messages}>
-                    {messagesElements}
+                <div className={styles.dialogs__messages}>
+                    <div>{messagesElements}</div>
 
                     {/*Create message*/}
 
-                    <div className={`${classes.messages} ${classes.messages__container}`}>
-                        <div className={classes.messages__create}>
+                    <div className={`${styles.messages} ${styles.messages__container}`}>
+                        <div className={styles.messages__create}>
                             <DialogsReduxForm onSubmit={onSendMessage}/>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
@@ -49,10 +42,10 @@ const Dialogs = ({dialogs, messages, send, isAuth}) => {
 
 const DialogsForm = ({handleSubmit}) => {
     return (
-        <form onSubmit={handleSubmit}>
-            <Field className={classes.messages__text} component={Textarea}
-                   name={"newMessage"} type={"text"} validate={[required, maxLength30]}/>
-            <button className={classes.messages__send}>Send</button>
+        <form className={styles.messages__form} onSubmit={handleSubmit}>
+            {createField("Type your message", "newMessage", [required, maxLength100],
+                Textarea, {}, "text", [styles.messages__text])}
+            <button className={styles.messages__send}>Send</button>
         </form>
     );
 }
--- src/components/dialogs/dialogItem/DialogItem.jsx
+++ src/components/dialogs/dialogItem/DialogItem.jsx
@@ -5,16 +5,15 @@ import {NavLink} from "react-router-dom";
 const DialogItem = ({id, name}) => {
     let path = "/dialogs/" + id;
     return (
-        <div className={classes.dialogs__dialog}>
-            <div className={classes.person__avatar}>
-                <img src="https://cdn.britannica.com/q:60/92/181392-050-08187DB5/cat-British-Shorthair-grey.jpg" alt=""/>
-            </div>
-            <div className={classes.person__name}>
-                <NavLink className={classes.person__text} to={path}>{name}</NavLink>
-            </div>
-        </div>
+        <NavLink to={path} className={classes.dialogs__dialog}>
+                <div className={classes.person__avatar}>
+                    <img src="https://cdn.britannica.com/q:60/92/181392-050-08187DB5/cat-British-Shorthair-grey.jpg" alt=""/>
+                </div>
+                <div className={classes.person__name}>
+                    <div className={classes.person__text}>{name}</div>
+                </div>
+        </NavLink>
     );
 }
-
 
 export default DialogItem;
